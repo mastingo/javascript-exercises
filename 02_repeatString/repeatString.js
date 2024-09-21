4a4b8a0db6771421b2b7b4eeead3324b0fe4686b
mastingo <mativokyle@gmail.com>
2024-09-21 13:35:52 +0300

    4-6

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function( string, num) {
+    word = ''
+    if(num < 0 ){
+        return('ERROR')
+    }
+    for(let i = 0; i < num; i++){
+        word = word + string
+    }
+    return word
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    if(!Number.isInteger(arg1) || !Number.isInteger(arg2)){
+        return('ERROR')
+    }
+    if(arg1 < 0 || arg2 < 0){
+    return('ERROR')
+    }
 
+    if( arg1 > arg2 ){
+        const arg3 = arg1;
+        arg1 = arg2;
+        arg2 = arg3
+    }
+
+    let sum = 0
+    for(let i = arg1; i <= arg2; i++ ){
+    sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
