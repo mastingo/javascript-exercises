25fde7460c89dc6d0af0e85cffcc4a9b99641564
mastingo <mativokyle@gmail.com>
2024-09-21 14:26:15 +0300

    7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(arg1) {
+  celcius = (arg1 - 32) * 5/9
+  return Math.round(celcius * 10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(arg1) {
+  farenheiht = (arg1 * 9/5) + 32
+  return Math.round(farenheiht * 10)/10
 };
 
 // Do not edit below this line
